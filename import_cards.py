0bdad758267b0e419c538389f51359f86943fdc0
James Forrest <jforrest@diaxion.com>
2017-05-08 15:59:04 +1000

    prints keys, and starting to look at values

--- import_cards.py
+++ import_cards.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python
 # imports the cards into the database
+import json
+
+allsets = "AllSets-x.json"
+allcards = "AllCards-x.json"
+
+# open and load the file
+with open(allsets) as sets_file:
+    sets = json.load(sets_file)
+
+# for keys, values in sets.keys():
+#    print keys
+#    print values["name"]
