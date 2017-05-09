b79f2c05cede82dbdccbc91ee5f8e88fc1d7c3c2
James Forrest <jforrest@diaxion.com>
2017-05-09 12:16:01 +1000

    working on SQL inserts for the Sets

--- import_cards.py
+++ import_cards.py
@@ -1,14 +1,36 @@
 #!/usr/bin/python
 # imports the cards into the database
 import json
+import mysql.connector
 
+# set some variables
 allsets = "AllSets-x.json"
 allcards = "AllCards-x.json"
+
+# connection dictionary
+config = {
+    'user': 'jforrest',
+    'password': 'password',
+    'host': 'mysql-dev',
+    'database': 'mtg',
+    'raise_on_warnings': True,
+}
+
+# connect to DB
+conn = mysql.connector.connect(**config)
 
 # open and load the file
 with open(allsets) as sets_file:
     sets = json.load(sets_file)
 
-# for keys, values in sets.keys():
-#    print keys
-#    print values["name"]
+setsdb = conn.cursor()
+
+for keys in sets.keys():
+    #    print keys
+    #    print sets[keys]["name"]
+    setsdb.execute("INSERT INTO Sets (Set_Code,Set_Name) VALUES (%s,%s)", keys, sets[keys]["name"])
+
+conn.commit()
+setsdb.close()
+
+conn.close()
