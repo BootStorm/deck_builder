d374e16ee0cc8b6784251be136c5c9cf407897ae
James Forrest <jforrest@diaxion.com>
2017-05-12 11:20:00 +1000

    started on the importing into cardset db. how to import multiple entries?

--- import_cards.py
+++ import_cards.py
@@ -19,20 +19,26 @@ config = {
 # connect to DB
 conn = mysql.connector.connect(**config)
 
-# open and load the file
+# open and load the files
 with open(allsets) as sets_file:
     sets = json.load(sets_file)
 
-setsdb = conn.cursor()
+with open(allcards) as cards_file:
+    cards = json.load(cards_file)
+
+db = conn.cursor()
 
 for keys in sets.keys():
     #    print keys
     #    print sets[keys]["name"]
     # if key exists will replace it, otherwise will insert new one.
-    setsdb.execute("REPLACE INTO Sets (Set_Code,Set_Name) VALUES (%s,%s)", (keys, sets[keys]["name"]))
+    db.execute("REPLACE INTO Sets (Set_Code,Set_Name) VALUES (%s,%s)", (keys, sets[keys]["name"]))
 
+for keys in cards.keys():
+    # need to sort out how to add in array.
+    db.execute("REPLACE INTO Card_DB (Card_Name,Card_Set) VALUES (%s,%s)", (keys, cards[keys][""]))
 
 conn.commit()
-setsdb.close()
+db.close()
 
 conn.close()
